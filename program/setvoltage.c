3a05e85a07046997c016d0af9eb2ae03cb7c1750
itoho <daiki110touhou@gmail.com>
2020-04-05 10:02:12 +0900

    add program/setvoltage.c

---
+++ program/setvoltage.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#define BIT 8
+void outX(int x){
+	for(int i=0;i<=BIT-1;i++){
+		digitalWrite(i,(x&1U));
+		x=x>>1;
+	}
+}
+void outY(int y){
+        for(int i=0;i<=BIT-1;i++){
+                digitalWrite(i+8, (x&1U));
+                y=y>>1;
+        }
+}
+void main(void){
+int value;
+printf("input X value(0-255)...");
+scanf("%d", &value);
+outX(value);
+printf("input Y value(0-255)...");
+scanf("%d", &value);
+}
